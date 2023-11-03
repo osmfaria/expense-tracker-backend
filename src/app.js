de1571e3ac1f2efa79e302de354d1760d18ecc19
Osmar Faria <osm.faria@gmail.com>
2023-11-03 11:44:55 -0700

    refactor: removed app listen

--- src/app.js
+++ src/app.js
@@ -2,15 +2,9 @@ import express from 'express'
 import appRoutes from './routes/index.js'
 import errorMiddleware from './middleware/errorMiddleware.js'
 
-const app = express()
+export const app = express()
 app.use(express.json())
 
 appRoutes(app)
 
 app.use(errorMiddleware)
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-  console.log(`🌎 server running ${PORT}`)
-})
