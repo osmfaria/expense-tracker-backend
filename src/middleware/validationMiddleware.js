dbf3caafe7f9fa4b2ce36214ac9e0c30e12f3b40
Osmar Faria <osm.faria@gmail.com>
2023-11-02 23:02:48 -0700

    feat: implement middleware to validate schemas

---
+++ src/middleware/validationMiddleware.js
@@ -0,0 +1,17 @@
+import { AppError } from '../../errors/appError.js'
+
+export const validate = (schema) => async (req, res, next) => {
+  try {
+    const validatedData = await schema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    })
+
+    req.validatedData = validatedData
+
+    next()
+  } catch (err) {
+    console.log(err)
+    next(new AppError(err.errors, err.statusCode))
+  }
+}
