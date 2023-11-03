d3c7f613a3728f93faaaa91e5b20e8238e771e80
Osmar Faria <osm.faria@gmail.com>
2023-11-02 23:39:14 -0700

    feat: add list users controller for requests

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 import createUserService from '../service/user/createUser.service.js'
+import listUsersService from '../service/user/listUsers.service.js'
 
 export const createUserController = async (req, res) => {
   const data = req.validatedData
@@ -6,4 +7,10 @@ export const createUserController = async (req, res) => {
   const user = await createUserService(data)
 
   return res.status(201).json(user)
+}
+
+export const listUsersController = async (req, res) => {
+  const users = await listUsersService()
+
+  return res.status(200).json(users)
 }
