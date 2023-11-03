44dcb6b5f0503129d151f9d354420dc6a6615b9a
Osmar Faria <osm.faria@gmail.com>
2023-11-02 23:40:31 -0700

    feat: implement list users route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { createUserController } from '../controller/userController.js'
+import {
+  createUserController,
+  listUsersController,
+} from '../controller/userController.js'
 import { validate } from '../middleware/validationMiddleware.js'
 import { userCreateSchema } from '../schema/userSchema.js'
 
 const userRouter = Router()
 
 userRouter.post('', validate(userCreateSchema), createUserController)
+userRouter.get('', listUsersController)
 
 export default userRouter
