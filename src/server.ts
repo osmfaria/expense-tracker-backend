f96b3185cb9197f241dfb81098c106f3c642b2a7
Osmar Faria <osm.faria@gmail.com>
2023-11-03 19:53:38 -0700

    refactor: remove comments and organize

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from './app'
-;(() => {
-  const PORT = process.env.PORT || 5000
-  app.listen(PORT, () => {
-    console.log(`🌎 Server running on port ${PORT}`) // Fixed to use console.log for output
-  })
-})()
