79e700965f8597b223f204d9063ab707ff769d17
Osmar Faria <osm.faria@gmail.com>
2023-11-04 06:40:19 -0700

    feat: implement expense update validation schema

--- src/schema/expenseSchema.js
+++ src/schema/expenseSchema.js
@@ -10,3 +10,13 @@ export const expenseCreateSchema = yup.object().shape({
     .test((value) => (value.toString().split('.')[1] || '').length <= 2),
   userId: yup.string().required(),
 })
+
+export const expenseUpdateSchema = yup.object().shape({
+  date: yup.date(),
+  description: yup.string().min(3).max(200),
+  amount: yup
+    .number()
+    .positive()
+    .test((value) => (value.toString().split('.')[1] || '').length <= 2),
+  id: yup.string().required(),
+})
