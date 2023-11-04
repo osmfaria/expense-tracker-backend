d659bff09895ff1c317a44efda7f0fd2e9f1a467
Osmar Faria <osm.faria@gmail.com>
2023-11-04 06:12:30 -0700

    feat: include expense route on index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
+import expenseRoute from './expenseRoutes.js'
 import userRouter from './userRoutes.js'
 
 const appRoutes = (app) => {
   app.use('/users', userRouter)
+  app.use('/expenses', expenseRoute)
 }
 
 export default appRoutes
