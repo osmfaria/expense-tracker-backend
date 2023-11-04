f47da8ab34dba2f614cbb655a930773d956941f1
Osmar Faria <osm.faria@gmail.com>
2023-11-04 07:12:29 -0700

    feat: implement expense delete controller

--- src/controller/expenseControler.js
+++ src/controller/expenseControler.js
@@ -1,4 +1,5 @@
 import createExpenseService from '../service/expense/createExpense.service'
+import deleteExpenseService from '../service/expense/deleteExpense.service'
 import updateExpenseService from '../service/expense/updateExpense.service'
 
 export const createExpenseController = async (req, res) => {
@@ -15,4 +16,12 @@ export const updateExpenseController = async (req, res) => {
   const expense = await updateExpenseService(data, id)
 
   return res.status(200).json(expense)
+}
+
+export const deleteExpenseController = async (req, res) => {
+  const { expense_id } = req.params
+
+  await deleteExpenseService(expense_id)
+
+  return res.status(204).send()
 }
