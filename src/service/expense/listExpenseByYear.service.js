7e658d3db750a0c97a347e11e02c33e692b5c3d0
Osmar Faria <osm.faria@gmail.com>
2023-11-04 21:18:14 -0700

    feat: implement list years with expense service

---
+++ src/service/expense/listExpenseByYear.service.js
@@ -0,0 +1,19 @@
+import prismaClient from '../../database/prismaClient'
+
+const listExpenseByYearService = async (user_id) => {
+  const expense = await prismaClient.$queryRaw`
+  SELECT EXTRACT(YEAR FROM date) as year,
+    SUM(amount) as total_amount
+  FROM 
+    public."Expense"
+  WHERE 
+    public."Expense"."userId" = ${user_id}
+  GROUP BY 
+    year
+  ORDER BY 
+    year DESC;
+  `
+  return expense
+}
+
+export default listExpenseByYearService
