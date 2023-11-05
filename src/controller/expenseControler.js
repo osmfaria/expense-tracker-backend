9d078f87094f8e2bb4d181896054b8a2d8cfcf91
Osmar Faria <osm.faria@gmail.com>
2023-11-04 20:30:43 -0700

    feat: implement list expense controller

--- src/controller/expenseControler.js
+++ src/controller/expenseControler.js
@@ -1,5 +1,7 @@
 import createExpenseService from '../service/expense/createExpense.service'
 import deleteExpenseService from '../service/expense/deleteExpense.service'
+import listExpenseService from '../service/expense/listExpense.service'
+import { listExpenseByWeekService } from '../service/expense/listExpenseByWeek.service'
 import updateExpenseService from '../service/expense/updateExpense.service'
 
 export const createExpenseController = async (req, res) => {
@@ -24,4 +26,20 @@ export const deleteExpenseController = async (req, res) => {
   await deleteExpenseService(expense_id)
 
   return res.status(204).send()
+}
+
+export const listExpenseController = async (req, res) => {
+  const { user_id, year } = req.params
+
+  const expense = await listExpenseService(user_id, year)
+
+  return res.status(200).json(expense)
+}
+
+export const listExpenseByWeekController = async (req, res) => {
+  const { user_id, year } = req.params
+
+  const expenses = await listExpenseByWeekService(user_id, year)
+
+  return res.status(200).json(expenses)
 }
