2ad88ce0317696871a10089806bcd4d928593017
Osmar Faria <osm.faria@gmail.com>
2023-11-04 20:25:25 -0700

    feat: implement list expense service

---
+++ src/service/expense/listExpense.service.js
@@ -0,0 +1,20 @@
+import prismaClient from '../../database/prismaClient'
+
+const listExpenseService = async (user_id, year) => {
+  const expenses = await prismaClient.expense.findMany({
+    where: {
+      userId: user_id,
+      date: {
+        gte: new Date(`${year}-01-01`),
+        lte: new Date(`${year}-12-31`),
+      },
+    },
+    orderBy: {
+      date: 'desc',
+    },
+  })
+
+  return expenses
+}
+
+export default listExpenseService
