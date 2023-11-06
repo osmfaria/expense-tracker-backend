360271f25ddd8ff9d7e1f2d90fdf386dbcd0133b
Osmar Faria <osm.faria@gmail.com>
2023-11-06 09:22:24 -0800

    fix: cors import

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import appRoutes from './routes/index.js'
 import errorMiddleware from './middleware/errorMiddleware.js'
-
-const cors = require('cors')
+import cors from 'cors'
 
 export const app = express()
 app.use(cors())
