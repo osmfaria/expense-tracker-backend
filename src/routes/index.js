8d3744cf8531b84ba565a0244d472dc1eb8fa116
Osmar Faria <osm.faria@gmail.com>
2023-11-05 16:49:30 -0800

    feat: add route for api docs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 import expenseRoute from './expenseRoutes.js'
 import userRouter from './userRoutes.js'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from '../../swagger.json'
 
 const appRoutes = (app) => {
   app.use('/users', userRouter)
   app.use('/expenses', expenseRoute)
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 }
 
 export default appRoutes
