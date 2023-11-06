bb2df327ec670b7f813b91bd8b62e57b9432ca78
Osmar Faria <osm.faria@gmail.com>
2023-11-05 20:16:09 -0800

    feat: implement error handling for expense not found

--- src/service/expense/updateExpense.service.js
+++ src/service/expense/updateExpense.service.js
@@ -1,14 +1,25 @@
+import { AppError } from '../../../errors/appError'
 import prismaClient from '../../database/prismaClient'
 
 const updateExpenseService = async (data, id) => {
-  const service = prismaClient.expense.update({
+  const expense = await prismaClient.expense.findUnique({
+    where: {
+      id,
+    },
+  })
+
+  if (!expense) {
+    throw new AppError('No expense found', 404)
+  }
+
+  const updatedExpense = prismaClient.expense.update({
     where: {
       id,
     },
     data,
   })
 
-  return service
+  return updatedExpense
 }
 
 export default updateExpenseService
