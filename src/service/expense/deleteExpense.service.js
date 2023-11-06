f4c6f7c6c892989e6b0672d8b7362b249f2d605b
Osmar Faria <osm.faria@gmail.com>
2023-11-05 20:14:32 -0800

    feat: implement error handling for expense  not found

--- src/service/expense/deleteExpense.service.js
+++ src/service/expense/deleteExpense.service.js
@@ -1,6 +1,17 @@
+import { AppError } from '../../../errors/appError'
 import prismaClient from '../../database/prismaClient'
 
 const deleteExpenseService = async (expense_id) => {
+  const expense = await prismaClient.expense.findUnique({
+    where: {
+      id: expense_id,
+    },
+  })
+
+  if (!expense) {
+    throw new AppError('Expense not found', 404)
+  }
+
   await prismaClient.expense.delete({
     where: {
       id: expense_id,
