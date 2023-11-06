e4b97e1ef235d671faf8eb890c0af5c279a71ae9
Osmar Faria <osm.faria@gmail.com>
2023-11-05 20:15:33 -0800

    feat: implement error handling for user not found

--- src/service/expense/listYearsWithExpense.service.js
+++ src/service/expense/listYearsWithExpense.service.js
@@ -1,6 +1,17 @@
+import { AppError } from '../../../errors/appError'
 import prismaClient from '../../database/prismaClient'
 
 const ListYearsWithExpenseService = async (user_id) => {
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: user_id,
+    },
+  })
+
+  if (!user) {
+    throw new AppError('User not found', 404)
+  }
+
   const expense = await prismaClient.$queryRaw`
   SELECT EXTRACT(YEAR FROM date) as year,
     SUM(amount) as total_amount
