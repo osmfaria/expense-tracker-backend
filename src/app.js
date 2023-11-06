727aaedd2503ffa9609877b0158a31d51934d111
Osmar Faria <osm.faria@gmail.com>
2023-11-06 08:11:21 -0800

    feat: add cors support

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ import 'express-async-errors'
 import appRoutes from './routes/index.js'
 import errorMiddleware from './middleware/errorMiddleware.js'
 
+const cors = require('cors')
+
 export const app = express()
+app.use(cors())
 app.use(express.json())
 
 appRoutes(app)
