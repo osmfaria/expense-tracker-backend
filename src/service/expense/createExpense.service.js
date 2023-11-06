38ba1fa38d877ecf1f59fd479b6777fe743f1d3b
Osmar Faria <osm.faria@gmail.com>
2023-11-05 20:14:03 -0800

    feat: implement error handling for user not found

--- src/service/expense/createExpense.service.js
+++ src/service/expense/createExpense.service.js
@@ -1,6 +1,17 @@
 import prismaClient from '../../database/prismaClient'
+import { AppError } from '../../../errors/appError'
 
 const createExpenseService = async (data, userId) => {
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id: userId,
+    },
+  })
+
+  if (!user) {
+    throw new AppError('User not found', 404)
+  }
+
   const expense = await prismaClient.expense.create({
     data: {
       ...data,
